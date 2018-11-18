529734504de1e387006fee64273de947fd37b8c0
Zanderson0002 <zanderson0002@kctcs.edu>
2018-11-18 13:39:27 -0500

    Added Canvas popup to display eCard images

--- Ecard Generator Option Menu.py
+++ Ecard Generator Option Menu.py
@@ -19,10 +19,27 @@ occasions = OptionMenu(window, selection, *choices).place(x=90, y=30)
 #Function to get selected occasion(It just prints now so I knew it returned correctly, we would assign it to a variable later)
 def getSelection():
     print("Occasion is:" + selection.get())
+    eCard()
 
 
 button = Button(window, text="OK", command=getSelection).place(x=125, y=65)
 
+# Second window with Canvas to display E-Card
+class eCard:
+    def __init__(self):
+        eCard = Tk()  # Create window
+        eCard.title("E-Card Generator") # Window title
+
+        # Set canvas
+        self.canvas = Canvas(eCard, width = 592, height = 420)
+        self.canvas.pack()
+
+        # Images must be initialized in 'init' to be called in functions
+        ##self.testPhoto = PhotoImage(file = 'Test.gif')  # Placeholder for image files (can be GIF and PGM/PPM)
+
+        # Buttons in frame
+        frame = Frame(eCard)
+        frame.pack()
+
 
 mainloop()
-
